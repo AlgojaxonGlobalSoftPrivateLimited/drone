fd7358cd2306b9b57e1dd567b5bd15b798646dde
Algojaxon <146240158+AlgojaxonGlobalSoftPrivateLimited@users.noreply.github.com>
2023-12-19 12:36:03 +0530

    Update flaskapp.py

--- flaskapp.py
+++ flaskapp.py
@@ -1,48 +1,50 @@
-from flask import Flask, render_template, Response,jsonify,request,session
-from flask_wtf import FlaskForm
+from flask import Flask, render_template, Response, request
+from flask_socketio import SocketIO, emit
 import secrets
-from wtforms import FileField, SubmitField,StringField,DecimalRangeField,IntegerRangeField
-from werkzeug.utils import secure_filename
-from wtforms.validators import InputRequired,NumberRange
-import os
-import cv2
 from YOLO_Video import generate_frames_web
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
+socketio = SocketIO(app)
 
+# The variable to store the latest video data
+latest_video_data = None
 
-@app.route('/', methods=['GET','POST'])
-@app.route('/home', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-    session.clear()
     return render_template('indexproject.html')
 
-@app.route('/FrontPage', methods=['GET','POST'])
+@app.route('/FrontPage', methods=['GET', 'POST'])
 def frontpage():
-    session.clear()
     return render_template('videoprojectnew.html')
 
-
-@app.route("/webcam", methods=['GET','POST'])
+@app.route("/webcam", methods=['GET', 'POST'])
 def webcam():
-    session.clear()
     return render_template('ui.html')
 
-
-@app.route("/web", methods=['GET','POST'])
+@app.route("/web", methods=['GET', 'POST'])
 def web():
-    session.clear()
     return render_template('web.html')
 
+# Sender: Route for setting up video stream
+@app.route('/video_sender', methods=['POST'])
+def video_sender():
+    try:
+        global latest_video_data
+        video_data = request.data
+        latest_video_data = video_data
+        # Emit each frame to the 'receiver' room using WebSocket
+        socketio.emit('frame', {'video_data': video_data}, room='receiver')
+        return 'Video stream set up successfully'
+    except Exception as e:
+        return f'Error: {e}', 500
 
-
-# To display the Output Video on Webcam page
-@app.route('/webapp')
-def webapp():
-    rtsp_url = "rtsp://192.168.1.1/H264?W=720&H=400&FPS=30&BR=3000000/"
-    return Response(generate_frames_web(rtsp_url), mimetype='multipart/x-mixed-replace; boundary=frame')
-
+# Receiver: WebSocket route for receiving video stream
+@socketio.on('connect', namespace='/receiver')
+def handle_receiver_connect():
+    global latest_video_data
+    emit('frame', {'video_data': latest_video_data})
 
 if __name__ == "__main__":
-    app.run(debug=True,port=8082)
+    socketio.run(app, debug=True, port=8082)
